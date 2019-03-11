39f54cc235505dc1b61fab6904514e02df65a871
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-03-11 08:36:18 +0300

    Define queue object for transcoding tasks

--- transcoder/main.py
+++ transcoder/main.py
@@ -3,6 +3,7 @@ CLI application for transcoding music files
 '''
 
 
+import mutagen
 from ruamel import yaml
 
 from transcoder.encoders import (
@@ -15,6 +16,74 @@ def run():
     '''
     CLI entry point
     '''
+    # 1. Load config from YAML
+    # 2. Find relevant music files and add them to queue
+    # 3. Concurrently process each file in the queue:
+    #    - Calculate target location
+    #    - Transcode
+    #    - Fill tags
+    #    - Copy lyrics
+    #    - Copy cover art
+
+
+
+class TranscodingTask:
+    '''Stores information required to transcode a single music file'''
+
+    def __init__(self, filename, metadata=None):
+        self.source = filename
+        self.metadata = metadata.data if metadata else {}  # hods object
+        self.tags = self.read_tags()
+        self.path_elements = self.calculate_path_elements()
+
+
+    def __repr__(self):
+        return '{cls}({filename})'.format(
+            cls = self.__class__.__name__,
+            filename = self.source,
+        )
+
+
+    def read_tags(self):
+        '''Read music tags from file headers'''
+        # Values from metadata file are ignored intentionally:
+        #  - There is no point using "Various Artists" placeholder for
+        #    individual tracks
+        #  - Track title and album name are assumed to be correct in source
+        #    tags
+        #  - Genre, year and other less important tags are not worth the
+        #    special treatment
+        tags = mutagen.File(self.source, easy=True).tags
+        return tags
+
+
+    def calculate_path_elements(self):
+        '''Calculate target path elements for transcoding this file'''
+        allowed_fields = {
+            'artist',
+            'album',
+            'year',
+            'number',
+            'title',
+            'genre',
+        }
+
+        elements = {}
+        for field in allowed_fields:
+            meta_value = self.metadata.get(field)
+            if meta_value:
+                elements[field] = value(meta_value)
+                continue
+
+            if field == 'number': field = 'tracknumber'
+            tag_value = self.tags.get(field)
+            if tag_value:
+                elements[field] = value(tag_value)
+                # TODO: add zero-padding for tracknumber
+                continue
+
+        return elements
+
 
 
 class TranscodingJob:
@@ -56,6 +125,20 @@ class TranscodingJob:
             cls=self.__class__.__name__,
             config=self.config_file,
         )
+
+
+
+def value(string_or_list):
+    '''
+    Get string value from a variable that might contain string or list of
+    strings
+    '''
+    if isinstance(string_or_list, str):
+        return string_or_list
+    elif len(string_or_list) == 1:
+        return string_or_list[0]
+    else:
+        return ', '.join(string_or_list)
 
 
 def find_files(directory):
