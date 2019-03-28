073243ff6e470f3b8b92cf2551902385653e435d
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-03-28 18:28:56 +0300

    WIP: Fetch texts from LyricsWiki

--- goodies/lyrics.py
+++ goodies/lyrics.py
@@ -2,9 +2,70 @@
 Fetch lyrics from public collections
 '''
 
+import re
+from urllib.parse import quote
+
+from lxml import etree
 
 from goodies.fetch import BaseDataFetcher
 
 
-def LyricsWikiFetcher(BaseDataFetcher):
+
+class BaseLyricsFetcher(BaseDataFetcher):
+    '''Base class for all lyrics fetchers'''
+
+    url_pattern = NotImplemented
+    _regex = {
+        'the_start': re.compile(r'^\s*the\s+(.*)', re.IGNORECASE),
+        'the_end': re.compile(r'(.*)[,\s]\s*the\s*$', re.IGNORECASE),
+    }
+
+
+    @classmethod
+    def fix_the(cls, caption, position='start'):
+        '''
+        Fix location of the "The" in a given caption
+
+        If "the" is present, it will be moved either to the start or to the end
+        of line according to the value of `position`
+        '''
+        regex = cls._regex
+        if position == 'start':
+            return regex['the_end'].sub(r'The \1', caption)
+        elif position == 'end':
+            return regex['the_start'].sub(r'\1, the', caption)
+        elif position is None:
+            for key in ('the_start', 'the_end'):
+                if regex[key].match(caption):
+                    return regex[key].sub(r'\1', caption)
+            return caption
+        else:
+            raise ValueError('invalid argument value: position={}'.format(position))
+
+
+class LyricsWikiFetcher(BaseLyricsFetcher):
     '''Fetch lyrics from Lyrics Wiki'''
+
+    api = 'http://lyrics.wikia.com/api.php'
+    marker = re.compile(r'^.*<lyrics>(.*)</lyrics>.*$', re.DOTALL)
+
+    def fetch(self, artist, title):
+        api_response = self.get(self.api, params=dict(
+            action = 'lyrics',
+            func = 'getSong',
+            fmt = 'xml',  # JSON output is malformed! Single quotes all over the place
+            artist = self.fix_the(artist),
+            song = title,
+        ))
+
+        overview = etree.fromstring(api_response.content)
+        if not overview.xpath('page_id//text()'):
+            return None  # lyrics not found
+
+        full_page_url = overview.xpath('url//text()')[0]
+        html = self.parse_html(full_page_url, params=dict(action='edit'))
+        wiki_text = html.xpath('//*[@id="wpTextbox1"]//text()')[0]
+        lyrics = self.marker.sub(r'\1', wiki_text)
+        # TODO: handle inline html tags in lyrics
+        # TODO: drop "we are not licensed to display"
+        return lyrics
