44dafa6921fcc46e42a83798db94f68f2275ea51
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-04-01 17:37:39 +0300

    Form a proper Python package: add license and setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='musicbatch',
+    version='0.0.1',
+    description='Batch processing of music files',
+    url='https://github.com/sio/musicbatch',
+    author='Vitaly Potyarkin',
+    author_email='sio.wtf@gmail.com',
+    license='Apache',
+    platforms='any',
+    entry_points={
+        'console_scripts': [
+            'transcoder=musicbatch.transcoder.main:run',
+            'lyrics=musicbatch.lyrics.app:run',
+        ],
+    },
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    install_requires=[
+        # Transcoder
+        'mutagen',
+        'pillow',
+        'pydub',
+        'ruamel.yaml',
+        'https://github.com/sio/hods/archive/master.zip#egg=hods',
+        # Lyrics + Scrapehelper
+        'requests',
+        'lxml',
+        'sqlalchemy',
+    ],
+    python_requires='>=3.3',
+    zip_safe=True,
+)
