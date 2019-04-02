783e53bead5281f72d08a44ca3f53a214dd4a50d
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-04-02 11:51:27 +0300

    Install hods via direct link automatically

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import sys
+
+
+class InstallDependenciesByDirectLink(install):
+    direct_links = [
+        'https://github.com/sio/hods/archive/master.zip#egg=hods',
+    ]
+    def run(self):
+        if self.direct_links:
+            subprocess.call(
+                [sys.executable, '-m', 'pip', 'install'] + self.direct_links
+            )
+        super().run()
+
 
 setup(
     name='musicbatch',
@@ -17,17 +33,18 @@ setup(
     },
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
+    cmdclass={'install': InstallDependenciesByDirectLink},
     install_requires=[
         # Transcoder
         'mutagen',
         'pillow',
         'pydub',
         'ruamel.yaml',
-        'hods',
         # Lyrics + Scrapehelper
         'requests',
         'lxml',
         'sqlalchemy',
+        # 'hods' package will be installed via direct link since it's not published on PyPA
     ],
     python_requires='>=3.3',
     zip_safe=True,
