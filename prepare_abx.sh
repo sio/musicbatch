99f0316a59172237c440b9e6b5e3c63a3a6230c6
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-04-05 16:35:29 +0300

    Use pipe instead of a temp file

--- prepare_abx.sh
+++ prepare_abx.sh
@@ -11,10 +11,7 @@ DEST_DIR="./abx-samples"
 
 SAMPLE_A="$DEST_DIR/original.wav"
 SAMPLE_B="$DEST_DIR/transcoded.wav"
-TEMPFILE="$DEST_DIR/tempfile"
 
 mkdir "$DEST_DIR" || true
 ffmpeg -i "$INPUT" "$SAMPLE_A"
-ffmpeg -i "$SAMPLE_A" $TRANSCODING_PARAMETERS "$TEMPFILE"
-ffmpeg -i "$TEMPFILE" "$SAMPLE_B"
-rm "$TEMPFILE"
+ffmpeg -i "$SAMPLE_A" $TRANSCODING_PARAMETERS - | ffmpeg -i - "$SAMPLE_B"
